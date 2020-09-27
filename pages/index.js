4f6c014157827713862bab121c6a7482fd783ebb
chadda chakib <chadda.chakib@gmail.com>
2020-09-27 20:41:51 +0200

    add typescript

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-import Head from "next/head";
-
-export default function Home() {
-  return <h1> Boutique </h1>;
-}
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  const i: number = 10;
+  const v = i + 10;
+  return <h1> Boutique {v} </h1>;
+}
